0801ffaa24c53751565f7e4909f00d3617238c50
Felicio Cassalho <61760479+felicio93@users.noreply.github.com>
2025-04-17 15:27:00 -0400

    Update collocate.py

--- model_sat/collocate.py
+++ model_sat/collocate.py
@@ -8,7 +8,7 @@ import ocsmesh
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(),  # Print logs to the console
